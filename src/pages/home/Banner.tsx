df897d5dec1bf49c1e9ec75f89da9ea29f4544eb
bijayneupane18 <bjneupane5@gmail.com>
2024-09-19 15:51:41 +0545

    component: Menu at banner bottom

--- src/pages/home/Banner.tsx
+++ src/pages/home/Banner.tsx
@@ -2,6 +2,7 @@ import { Button, ButtonProps, styled } from "@mui/material"
 import { teal } from "@mui/material/colors";
 import { IoCartOutline } from "react-icons/io5";
 import BannerModel from "../../assets/banner.png"
+import { Link } from "react-router-dom";
 
 const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
     color: theme.palette.getContrastText(teal[600]),
@@ -12,10 +13,34 @@ const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
     },
   }));
 
+  const navbarItems = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Showcase",
+      path: "/showcase",
+    },
+    {
+      name: "Product",
+      path: "/product",
+    },
+    {
+      name: "Offer",
+      path: "/offer",
+    },
+    {
+      name: "Job",
+      path: "/job",
+    },
+  ];
+
 const Banner = () => {
   return (
     <>
-        <div className="h-[500px] w-full flex gap-4 px-6 sm:px-10 md:px-14 lg:px-24 rounded-2xl bg-gradient-to-r from-orange-600 via-orange-400 to-orange-600">
+    <div className="relative mb-4 md:mb-16">
+        <div className="h-[440px] w-full flex gap-4 px-6 sm:px-10 md:px-14 lg:px-24 rounded-2xl bg-gradient-to-r from-orange-600 via-orange-400 to-orange-600">
             <div className="flex flex-col justify-center items-center md:items-start gap-4 h-full w-full md:w-2/3">
             <h1 className="text-5xl text-center md:text-left font-bold text-white md:tracking-wide">Lorem Ipsum is simply dummy</h1>
             <p className="text-lg text-white text-center md:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -25,6 +50,14 @@ const Banner = () => {
             <img src={BannerModel} alt="" className="h-full"/>
             </div>
         </div> 
+        <div className="hidden absolute -bottom-10 md:flex justify-center w-full items-center">
+        <ul className="shadow-xl rounded-2xl h-fit w-fit flex justify-center items-center bg-white">
+          {navbarItems.map((data, index)=>
+            <li key={index} className="font-bold md:px-10 lg:px-14 py-7 hover:bg-teal-600 rounded-lg"><Link to={data.path}>{data.name}</Link></li>
+          )}
+          </ul>
+        </div>
+        </div>
     </>
   )
 }
